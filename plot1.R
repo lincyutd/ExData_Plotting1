e65f58953aad4079b5bdfaffa5bc983cc78bfe0f
chenmeng <lincy090917@hotmail.com>
2015-12-13 18:15:58 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 dat <-read.table('household_power_consumption.txt',sep=';',header=TRUE,na.strings = '?')
 dat <-subset(dat,Date=='1/2/2007'|Date=='2/2/2007')
-dat[,'Date']<-as.Date(dat[,'Date'],format='%d/%m/%Y)
+dat[,'Date']<-as.Date(dat[,'Date'],format='%d/%m/%y)
 png(filename='plot1.png',width=480,height=480)
 hist(dat$Global_active_power,main='Global Active Power',xlab='Global Active Power(kilowatts)',col='red')
 dev.off()
